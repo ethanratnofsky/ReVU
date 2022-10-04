8f937c143e88e109061c6f624bae53b6732e87db
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2022-10-04 14:05:11 -0500

    Make dining hall buttons decrease opacity on press

--- frontend/components/homepage/DiningHallButton.js
+++ frontend/components/homepage/DiningHallButton.js
@@ -1,4 +1,4 @@
-import { Image, Pressable, Text, View } from 'react-native';
+import { Image, TouchableOpacity, Text, View } from 'react-native';
 
 import diningHallButtonStyles from './DiningHallButtonStyles';
 import { useNavigation } from '@react-navigation/native';
@@ -11,13 +11,13 @@ const DiningHallButton = ({ id, name, overallRating }) => {
     }
 
     return (
-        <Pressable style={diningHallButtonStyles.container} onPress={handlePress}>
+        <TouchableOpacity activeOpacity={0.6} style={diningHallButtonStyles.container} onPress={handlePress}>
             <Text style={diningHallButtonStyles.name}>{name}</Text>
             <View style={diningHallButtonStyles.ratingContainer}>
                 <Text style={diningHallButtonStyles.rating}>{overallRating === null ? 'Unrated' : overallRating}</Text>
                 {overallRating !== null && <Image style={diningHallButtonStyles.star} source={require('../../assets/images/gold-star.png')} />}
             </View>
-        </Pressable>
+        </TouchableOpacity>
     );
 }
 
