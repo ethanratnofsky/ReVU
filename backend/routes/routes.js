25c87249322b02bbe61a61fcec57f70b12ae28a5
Kevin Silvergold <ksilvergold18@gmail.com>
2022-10-10 16:22:47 -0500

    Updates

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -9,7 +9,7 @@ const RatingModel = require('../models/rating_model');
 module.exports = router;
 
 // Ratings
-router.post('/post', async (req, res) => {
+router.post('/post/newRating', async (req, res) => {
     const data = new RatingModel({
         userId: req.body.userId,
         diningHall: req.body.diningHall,
@@ -26,9 +26,10 @@ router.post('/post', async (req, res) => {
     }
 });
 
-router.get('/getAll/:userId', async (req, res) => {
+router.get('/getAll/userRatings/:userId', async (req, res) => {
     try{
-        const data = await RatingModel.findById(req.params.userId);
+        const data = await RatingModel
+            .find({userId: req.params.userId});
         res.json(data)
     }
     catch(error){
@@ -36,9 +37,11 @@ router.get('/getAll/:userId', async (req, res) => {
     }
 });
 
-router.get('/getAll/:diningHall', async (req, res) => {
+router.get('/getAll/foodRatings/:diningHall', async (req, res) => {
     try{
-        const data = await RatingModel.and([{type: 'food'}, {diningHall: req.params.diningHall}]);
+        const data = await RatingModel
+            .find({diningHall: req.params.diningHall})
+            .and([{type: 'food'}]);
         res.json(data)
     }
     catch(error){
@@ -46,10 +49,39 @@ router.get('/getAll/:diningHall', async (req, res) => {
     }
 });
 
+router.get('/getAll/trafficRatings/:diningHall', async (req, res) => {
+    try {
+        const data = await RatingModel
+            .find({diningHall: req.params.diningHall})
+            .and([{type: 'traffic'}]);
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message});
+    }
+});
 
+router.put('/put/updateRating', async (req, res) => {
+    try {
+        const result = await RatingModel
+            .find(req.params.userId)
+            .findByIdAndUpdate(req.params.ratingId,
+                               req.params.rating,
+                               {new: true});
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+});
+
+router.delete('/delete/allRatings', async (req, res) => {
+    const data = await RatingModel
+        .find();
+    res.send('All ratings have been deleted');
+})
 
 // Complaints
-router.post('/post', async (req, res) => {
+router.post('/post/newComplaint', async (req, res) => {
     const data = new ComplaintModel({
         userId: req.body.userId,
         contact: req.body.contact,
@@ -66,7 +98,7 @@ router.post('/post', async (req, res) => {
     }
 });
 
-router.get('/getAll', async (req, res) => {
+router.get('/getAll/complaints', async (req, res) => {
     try{
         const data = await ComplaintModel.find();
         res.json(data)
