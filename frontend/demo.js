2dd4833d78c81db9d4010b7e3a2f31c8df8f93ee
arjun724 <arjun.collur.menon@gmail.com>
2022-10-27 11:39:10 -0500

    Created demo.js file and made changes to comments.js

---
+++ frontend/demo.js
@@ -0,0 +1,173 @@
+
+export const DINING_HALLS = [
+    {
+        id: 0,
+        name: 'Rand Dining Hall',
+        overallRating: 5.0,
+    },
+    {
+        id: 1,
+        name: 'Kissam Dining Hall',
+        overallRating: 4.3,
+    },
+    {
+        id: 2,
+        name: 'Rothschild Dining Hall',
+        overallRating: 3.0,
+    },
+    {
+        id: 3,
+        name: 'E. B. I. Dining Hall',
+        overallRating: 4.2,
+    },
+    {
+        id: 4,
+        name: 'Zeppos Dining Hall',
+        overallRating: 2.5,
+    },
+    {
+        id: 5,
+        name: 'Commons Dining Hall',
+        overallRating: null,
+    }
+]
+export const COMPLAINTS = [
+    {
+        id: 1,
+        userID: 1,
+        contact: "arjun@yahoo.com",
+        content: 'This is a complaint',
+        timestamp: 1132,
+    },
+    {
+        id: 2,
+        userID: 2,
+        contact: "ethan@yahoo.com",
+        content: 'This is a complaint',
+        timestamp: 1132,
+    },
+    {
+        id: 3,
+        userID: 3,
+        contact: "shivam@yahoo.com",
+        content: 'This is a complaint',
+        timestamp: 1132,
+    },
+    {
+        id: 4,
+        userID: 4,
+        contact: "othergroupmember@yahoo.com",
+        content: 'This is a complaint',
+        timestamp: 1132,
+    },
+    {
+        id: 5,
+        userID: 5,
+        contact: "vikash@yahoo.com",
+        content: 'This is a complaint',
+        timestamp: 1132,
+    },
+    {
+        id: 6,
+        userID: 6,
+        contact: "pablo@yahoo.com",
+        content: 'This is a complaint',
+        timestamp: 1132,
+    }
+    
+]
+export const RATINGS = [
+    {
+        id: 1,
+        userID: 1,
+        diningID: 5,
+        type: 'food',
+        rating: 5,
+    },
+    {
+        id: 2,
+        userID: 2,
+        diningID: 3,
+        type: 'traffic',
+        rating: 1,
+    },
+    {
+        id: 3,
+        userID: 3,
+        diningID: 2,
+        type: 'food',
+        rating: 0,
+    },
+    {
+        id: 4,
+        userID: 4,
+        diningID: 1,
+        type: 'food',
+        rating: 4,
+    },
+    {
+        id: 5,
+        userID: 5,
+        diningID: 3,
+        type: 'traffic',
+        rating: 3,
+    },
+    {
+        id: 6,
+        userID: 6,
+        diningID: 0,
+        type: 'food',
+        rating: 0,
+    },
+]
+export const COMMENTS = [
+    {
+        id: 1,
+        userID: 1,
+        diningID: 5,
+        content: 'This is a comment',
+        timestamp: 1132,
+    },
+    {
+        id: 2,
+        userID: 2,
+        diningID: 3,
+        content: 'This is another comment',
+        timestamp: 1132,
+    },
+    {
+        id: 3,
+        userID: 3,
+        diningID: 2,
+        content: 'This is a really, really, really, really, really, really, really, really, really, really, really, really, really, really, really long comment',
+        timestamp: 1139,
+    },
+    {
+        id: 4,
+        userID: 4,
+        diningID: 1,
+        content: 'This is another comment',
+        timestamp: 1149,
+    },
+    {
+        id: 5,
+        userID: 5,
+        diningID: 3,
+        content: 'This is another comment',
+        timestamp: 1132,
+    },
+    {
+        id: 6,
+        userID: 6,
+        diningID: 0,
+        content: 'This is another comment',
+        timestamp: 1133,
+    },
+    {
+        id: 7,
+        userID: 7,
+        diningID: 4,
+        content: 'This is another comment',
+        timestamp: 1134,
+    },
+];
