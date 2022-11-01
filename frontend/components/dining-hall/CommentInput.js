afde303e8e066e82101fe5242576bea8ef6b634a
svohra02 <66923911+svohra02@users.noreply.github.com>
2022-10-31 22:33:27 -0500

    made some connections

--- frontend/components/dining-hall/CommentInput.js
+++ frontend/components/dining-hall/CommentInput.js
@@ -2,16 +2,52 @@ import { useState } from 'react';
 import { Alert, Image, Keyboard, KeyboardAvoidingView, TouchableOpacity, TextInput, View } from 'react-native';
 
 import commentInputStyles from './CommentInputStyles';
+const Filter = require('bad-words');
 
 const CommentInput = ({ diningHallId }) => {
     const [comment, setComment] = useState('');
+    const filter = new Filter({'placeHolder': '*'});
 
     const handleSubmit = () => {
-        Alert.alert('Comment Posted', comment); // TODO: post comment to backend
 
-        // TODO: only execute if comment was successfully posted
-        setComment('');
-        Keyboard.dismiss();
+        if (filter.isProfane(comment)) {
+            alert("Your comment has inappropriate language. Please resubmit.");
+        } else {
+            const requestOptions = {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({ diningHallId: diningHallId, userId: 1,  content: comment })
+            };
+
+            fetch('https://sleepy-reaches-22563.herokuapp.com/api/post/createComment', requestOptions)
+            .then(async response => {
+                const isJson = response.headers.get('content-type')?.includes('application/json');
+                const data = isJson && await response.json();
+
+                if (!response.ok) {
+                    const err = (data && data.message) || response.status;
+                    return Promise.reject(error);
+                }
+
+                setComment('');
+                Keyboard.dismiss();
+            }).catch(error => {
+                console.log(error);
+                alert("Comment did not submit properly. Retry later.");
+            });
+        }
+
+        // if (issue == null || contact == null || dining == null || urgency == null) {
+        //     alert("Please fill out all fields. It seems there are some that were left empty.")
+        // } else if (!(/\S+@\S+\.\S+/.test(contact))) {
+        //     alert("Email does not have proper format. Please re-enter email address.");
+        // } else if (filter.isProfane(issue)) {
+        //     alert("You're issue has inappropriate language. Please re-enter your issue input.");
+        // } else if (filter.isProfane(contact)) {
+        //     alert("Your email has inappropriate language. Please re-enter your email.");
+        // }
+        // Alert.alert('Comment Posted', comment); // TODO: post comment to backend
+
     };
 
     return (
--- frontend/components/dining-hall/Comments.js
+++ frontend/components/dining-hall/Comments.js
@@ -4,6 +4,8 @@ import commentsStyles from './CommentsStyles';
 
 import { COMMENTS, USERS } from '../../demo.js';
 
+import { useState } from 'react';
+
 const Comment = ({ userId, content, timestamp }) => {
     const author = USERS.find(user => user.id === userId);
     const time = new Date(timestamp).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
@@ -21,7 +23,32 @@ const Comment = ({ userId, content, timestamp }) => {
 
 const Comments = ({ diningHallId }) => {
     // TODO: Get comments from backend
-    const comments = COMMENTS.filter(comment => comment.diningHallId === diningHallId);
+    // const comments = COMMENTS.filter(comment => comment.diningHallId === diningHallId);
+    
+    const [comments, setComments] = useState([]);
+
+    const requestOptions = {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'}
+    };
+
+
+    fetch(`https://sleepy-reaches-22563.herokuapp.com/api/getAll/diningComments/${diningHallId}`, requestOptions)
+    .then(async response => {
+        const isJson = response.headers.get('content-type')?.includes('application/json');
+        const data = isJson && await response.json();
+
+        if (!response.ok) {
+            const err = (data && data.message) || response.status;
+            return Promise.reject(error);
+        }
+        setComments(data);
+
+
+    }).catch(error => {
+        console.log(error);
+        alert("Comments failed to load. Please try again later.");
+    });
 
     const renderComment = ({ item }) => {
         return <Comment {...item} />
