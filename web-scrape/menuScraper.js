88983a3759d3209b3c851eabe51d9d35d4b09f3f
Kevin Silvergold <ksilvergold18@gmail.com>
2022-11-07 00:35:56 -0600

    Got loops to work on chrome but not in vscode. Close to done.

--- web-scrape/menuScraper.js
+++ web-scrape/menuScraper.js
@@ -18,33 +18,43 @@ const scraperObject = {
 
         info = await page.evaluate(async () => {
             dining_hall_links = Array.from(Array(20)).map((_, i) => document.querySelector(`#cbo_nn_unitImages_${i + 1} > div > div > a`));
+            ret = []
+            dining_hall_links.forEach(async (hall) => {
+                await hall.click()
+                const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+                await delay(1000)
 
-            await dining_hall_links[0].click()
-            const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
-            await delay(1000)
-            menus = document.querySelector("#cbo_nn_menuTableDiv")
-            blocks = menus.childNodes[0].querySelector('div > div')
-            tmp = blocks.childNodes[0].querySelector('div:nth-child(1) > a')
-            await tmp.click()
-            await delay(1000) 
-            options = document.querySelector("#itemPanel > section > div.table-responsive.pt-3 > table > tbody")
-            menu_items = []
-            options.childNodes.forEach((option) => {
-                tmp = option.textContent
-                start = tmp.indexOf(".")
-                end = tmp.search(/[0-9]/)
-                ret = tmp.substr(start + 1, end - start - 1)
-                if (ret.length == 0) {
-                    ret = tmp
-                }
+                menus = document.querySelector("#cbo_nn_menuTableDiv")
 
-                menu_items.push(ret)
-            })
+                menus.childNodes.forEach((menu) => {
+                    blocks = menu.querySelector('div > div')
+                    blocks.childNodes.forEach(async (block) => {
+                        info = block.querySelector('div:nth-child(1) > a')
+                        await info.click();
+                        await delay(1000)
 
-            return menu_items
+                        options = document.querySelector("#itemPanel > section > div.table-responsive.pt-3 > table > tbody")
+                        menu_items = []
+
+                        options.childNodes.forEach((option) => {
+                            tmp = option.textContent
+                            start = tmp.indexOf(".")
+                            end = tmp.search(/[0-9]/)
+                            fin_str = tmp.substr(start + 1, end - start - 1)
+                            if (fin_str.length == 0) {
+                                fin_str = tmp
+                            }
+
+                            menu_items.push(fin_str)
+                        })
+
+                        ret.push({tmp : menu_items})
+                    });
+
+                })
+            });
+            return ret
         });
-
-        console.log(info)
     }
 }
 
