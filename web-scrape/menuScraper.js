f75d9b52be6d38a77cb4a7928f509a5fda7b3d36
Kevin Silvergold <ksilvergold18@gmail.com>
2022-11-15 23:32:02 -0600

    Works, needs formatting and result needs to be named

--- web-scrape/menuScraper.js
+++ web-scrape/menuScraper.js
@@ -2,80 +2,71 @@ const { TimeoutError } = require("puppeteer");
 
 const scraperObject = {
     url: 'https://netnutrition.cbord.com/nn-prod/vucampusdining',
+    
     async scraper(browser) {
-        let page = await browser.newPage();
-		console.log(`Navigating to ${this.url}...`);
-		// Navigate to the selected page
-		await page.goto(this.url);
+        url = 'https://netnutrition.cbord.com/nn-prod/vucampusdining'
+        async function helper(index) {
+        //async function scrape(index) {
+            let page = await browser.newPage();
+            console.log(`Navigating to ${this.url}...`);
+            await page.goto(this.url);
+            // Navigate to the selected page
 
-        await page.exposeFunction('delay', async () => {
-            await page.waitForNavigation({waitUntil: 'networkidle2'})
-        });
+            await page.exposeFunction('delay', async () => {
+                await page.waitForNavigation({waitUntil: 'networkidle2'})
+            });
 
-        await page.exposeFunction('waitForSelector', async (x) => {
-            await page.waitForSelector(x)
-        });
+            await page.exposeFunction('waitForSelector', async (x) => {
+                await page.waitForSelector(x)
+            });
 
-        page.on('dialog', async dialog => {
-            console.log(dialog.message())
-            await dialog.dismiss()
-        })
+            page.on('dialog', async dialog => {
+                console.log(dialog.message())
+                await dialog.dismiss()
+            })
 
-        info = await page.evaluate(async () => {
-            dining_halls = ["Rand Dining Center",
-            "The Kitchen at Kissam",
-            "Rothschild Dining Center - Contains Peanuts and Treenuts",
-            "E. Bronson Ingram Dining Center",
-            "Zeppos Dining",
-            "The Commons Dining Center"]
-            dining_hall_links = Array.from(Array(20))
-                        .map((_, i) => document.querySelector(`#cbo_nn_unitImages_${i + 1} > div > div > a`))
-                        .filter(ele => dining_halls.includes(ele.textContent))
-            ret = []
+            info = await page.evaluate(async (index) => {
+                dining_link = await document.querySelector(`#cbo_nn_unitImages_${1} > div > div > a`);
 
-            dining_hall_links.forEach(async (hall) => {
-                await hall.click()
-                await window.waitForSelector("#cbo_nn_menuTableDiv")
+                dining_hall = dining_link.textContent
                 
-                menus = await document.querySelector("#cbo_nn_menuTableDiv")
-                alert(hall.textContent)
-                alert(menus.textContent)
-
-
+                await dining_link.click()
+                await window.waitForSelector("#cbo_nn_menuTableDiv")
+                //delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+                //await delay(1000)
+                ret = []
+                console.log("PRE MENU")
+                menus = await document.querySelector("#cbo_nn_menuTableDiv").querySelector('div')
+                console.log("POST MENU")
+                
                 menus.childNodes.forEach(async (menu) => {
                     blocks = await menu.querySelector('div > div')
-                    blocks.childNodes.forEach(async (block) => {
-
-                        info = await block.querySelector('div:nth-child(1) > a')
-                        
-                        await info.click();
-                        await window.waitForSelector("#itemPanel > section > div.table-responsive.pt-3 > table > tbody")
-
-                        options = await document.querySelector("#itemPanel > section > div.table-responsive.pt-3 > table > tbody")
-                        menu_items = []
-
-                        options.childNodes.forEach(async (option) => {
-                            tmp = option.textContent
-                            start = tmp.indexOf(".")
-                            end = tmp.search(/[0-9]/)
-                            fin_str = tmp.substr(start + 1, end - start - 1)
-                            if (fin_str.length == 0) {
-                                fin_str = tmp
-                            }
-                            menu_items.push(fin_str)
+                    blocks.childNodes.forEach(async (inner) => {
+                        blocks.childNodes.forEach(async (block) => {
+                            inner = block.querySelector('a')
+                            console.log("PRE OPTIONS")
+                            await inner.click()
+                            //delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+                            //await delay(1000)
+                            console.log("POST OPTIONS")
+                            await window.waitForSelector("#itemPanel > section > div.table-responsive.pt-3 > table > tbody")
+                            table = document.querySelector("#itemPanel > section > div.table-responsive.pt-3 > table > tbody")
+                            console.log("PRE OPTIONS")
+                            table.childNodes.forEach((row) => {
+                                ret.push({dining_hall : row.textContent})
+                            })
+                            console.log("POST OPTIONS")
                         });
-                        ret.push(menu_items)
-                        alert(ret)
-                    })
+                    });
                 });
+                return ret
             });
-            return ret
-        });
-        console.log("START")
-        info.forEach((ele) => {
-            console.log(ele.textContent)
-        })
-        console.log("DONE")
+            return info;
+        }
+        console.log("Start")
+        val = await helper(1)
+        console.log(val)
+        console.log("End")
     }
 }
 
